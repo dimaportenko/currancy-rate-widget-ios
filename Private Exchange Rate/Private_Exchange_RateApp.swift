407acf6e12c5a1b182c50d8f92a62aed885cf721
Dima Portenko <troublesohard@gmail.com>
2023-05-08 17:45:59 +0300

    Initial Commit

---
+++ Private Exchange Rate/Private_Exchange_RateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Private_Exchange_RateApp.swift
+//  Private Exchange Rate
+//
+//  Created by Dmitriy Portenko on 08.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Private_Exchange_RateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Private Exchange RateUITests/Private_Exchange_RateUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Private_Exchange_RateUITestsLaunchTests.swift
+//  Private Exchange RateUITests
+//
+//  Created by Dmitriy Portenko on 08.05.2023.
+//
+
+import XCTest
+
+final class Private_Exchange_RateUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
