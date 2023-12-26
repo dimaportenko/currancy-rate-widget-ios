d72eae55a8fccd462595a6557c1c1b9ae82771c6
Dima Portenko <troublesohard@gmail.com>
2023-12-26 22:49:30 +0200

    update for ios 17

--- Private Exchange/Private_Exchange.swift
+++ Private Exchange/Private_Exchange.swift
@@ -73,7 +73,7 @@ struct Private_ExchangeEntryView : View {
                     .font(.footnote)
             }
         }
-        .padding()
+        .containerBackground(.secondary, for: .widget)
     }
 }
 
